e98144a19563200e86afba64c865d282aa37716e
Vinita Kumari <vinitaksoni0502@gmail.com>
2025-03-10 00:00:56 +0530

    Add validation to ensure input contains only numeric values

--- string_calculator/manipulate_string.rb
+++ string_calculator/manipulate_string.rb
@@ -6,6 +6,10 @@ class ManipulateString
     return 0 if numbers.empty?
 
     num_list = [numbers]
+
+    if num_list.any? { |n| n.to_s !~ /^-?\d+$/ }
+      raise ArgumentError, "Invalid input format: Only numbers are allowed"
+    end
     num_list = num_list.map(&:to_i)
 
     return num_list.sum
