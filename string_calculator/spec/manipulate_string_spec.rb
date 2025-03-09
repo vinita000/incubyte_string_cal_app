74fe4ce5d7f758d512538456c926726839e52c99
Vinita Kumari <vinitaksoni0502@gmail.com>
2025-03-09 23:38:07 +0530

    Add spec to raise ArgumentError when input is not a string

--- string_calculator/spec/manipulate_string_spec.rb
+++ string_calculator/spec/manipulate_string_spec.rb
@@ -1,11 +1,16 @@
 require_relative '../manipulate_string'
 
 RSpec.describe ManipulateString do
-  let(:string_calulate) { ManipulateString.new }
+  let(:string_calculate) { ManipulateString.new }
 
   it 'returns 0 for an empty string' do
-    expect(string_calulate.add('')).to eq(0)
+    expect(string_calculate.add('')).to eq(0)
   end
+
+  it 'raises an ArgumentError when input is not a integer' do
+    expect { string_calculate.add(123) }.to raise_error(ArgumentError, "Input must be a string")
+  end
+
 end
 
 
