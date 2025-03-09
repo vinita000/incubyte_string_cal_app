8c071801b89ac2ac75fe5a067aa72801cf0c3c13
Vinita Kumari <vinitaksoni0502@gmail.com>
2025-03-09 23:52:19 +0530

    Convert input string to integer and return sum

--- string_calculator/manipulate_string.rb
+++ string_calculator/manipulate_string.rb
@@ -2,7 +2,12 @@ class ManipulateString
 
   def add(numbers)
     raise ArgumentError, "Input must be a string" unless numbers.is_a?(String)
-    
+
     return 0 if numbers.empty?
+
+    num_list = [numbers]
+    num_list = num_list.map(&:to_i)
+
+    return num_list.sum
   end
 end
